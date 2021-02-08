004fad1062ccfcdd85fc1e49969bd1fdd6b75971
unknown <Salina Haque>
2021-02-08 11:53:33 +0600

    closure

---
+++ closure.js
@@ -0,0 +1,19 @@
+function clock()
+{
+    let count = 0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+const clock1 = clock();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = clock();
+console.log(clock2());
+console.log(clock2());
+
+console.log(clock1());
